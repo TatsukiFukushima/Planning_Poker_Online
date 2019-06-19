efeffdd7851a6fd4860ab99f2aedab212c211cf0
Fukushima Tatsuki <fukushimatatsuki@Fukushima-no-MacBook-Air.local>
2019-06-19 16:55:13 +0900

    edit user model (rails_tutorial 6)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i # emaliの型みたいなやつ
+  validates :email, presence: true, length: { maximum: 255 },
+                format: { with: VALID_EMAIL_REGEX },
+                uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
+
+  before_save { self.email = email.downcase }
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   # GET /users

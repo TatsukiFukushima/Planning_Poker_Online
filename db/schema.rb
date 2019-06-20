7624215f8c611c73d133ca1b5821efee89e496d3
Fukushima Tatsuki <fukushimatatsuki@Fukushima-no-MacBook-Air.local>
2019-06-20 10:44:09 +0900

    add sign_up

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190612092937) do
+ActiveRecord::Schema.define(version: 20190619072329) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end

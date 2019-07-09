e4f66e62463d4ff1e4377d5a1b6854537cb389c2
Fukushima Tatsuki <fukushimatatsuki@Fukushima-no-MacBook-Air.local>
2019-07-09 16:39:53 +0900

    [bugfix]WebSocketのバグを修正

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -4,8 +4,6 @@ class RoomChannel < ApplicationCable::Channel
   end
 
   def unsubscribed
-    subscription = Subscription.find_by(user_id: current_user.id, room_id: params['room_id'])
-    subscription.destroy
   end
 
   def speak(data)
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,8 @@
 class Message < ApplicationRecord
   validates :content, presence: true
-  after_create_commit { MessageBroadcastJob.perform_later self }
 
   belongs_to :sent_user, class_name: "User", foreign_key: 'user_id'
   belongs_to :room
+
+  after_create_commit { MessageBroadcastJob.perform_later self }
 end

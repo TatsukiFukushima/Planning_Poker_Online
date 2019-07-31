2cf7b10e90698d1b78c59a7c94d06fdf65934e85
Fukushima Tatsuki <fukushimatatsuki@Fukushima-no-MacBook-Air.local>
2019-07-31 11:50:15 +0900

    トップページがちょっとダサかったので変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,12 +2,12 @@ module ApplicationHelper
 
   # ページごとの完全なタイトルを返します。
   def full_title(page_title = '')
-    base_title = "Playing Poker Online"
+    base_title = "Planning Poker Online"
     if page_title.empty?
       base_title
     else
       page_title + " | " + base_title
     end
   end
-  
+
 end
